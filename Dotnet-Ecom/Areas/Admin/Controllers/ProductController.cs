d6e1de2d44c725e6ba947d9da5ef53dfc5c9edea
unknown <atn1908@outlook.com>
2023-07-03 10:07:33 -0500

    add foreign key for category and product

---
+++ Dotnet-Ecom/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,98 @@
+﻿using Ecom.DataAccess.Repository.IRepository;
+using Ecom.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dotnet_Ecom.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Product> products = _unitOfWork.Product.GetAll().ToList();
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product obj)
+        {
+            if (obj.Title == obj.Description)
+            {
+                ModelState.AddModelError("Name", "The Description cannot exactly match the Title.");
+            }
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product Created Successfully.";
+                return RedirectToAction("Index", "Product");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product? ProductFromDb = _unitOfWork.Product.Get(c => c.Id == id);
+            if (ProductFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(ProductFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product Updated Successfully.";
+                return RedirectToAction("Index", "Product");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product? ProductFromDb = _unitOfWork.Product.Get(c => c.Id == id);
+            if (ProductFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(ProductFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Product? obj = _unitOfWork.Product.Get(c => c.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Product Deleted Successfully.";
+            return RedirectToAction("Index", "Product");
+        }
+    }
+}
---
+++ Ecom.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using Ecom.DataAccess.Data;
+using Ecom.DataAccess.Repository.IRepository;
+using Ecom.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        void IProductRepository.Update(Product product)
+        {
+            _db.Products.Update(product);
+        }
+    }
+}
