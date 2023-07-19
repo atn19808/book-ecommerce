8c1521b51f123260224a559399e4cb5ec9cd085e
unknown <atn1908@outlook.com>
2023-07-19 00:00:26 -0500

    update url to work with azure

--- Dotnet-Ecom/Areas/Customer/Controllers/CartController.cs
+++ Dotnet-Ecom/Areas/Customer/Controllers/CartController.cs
@@ -125,7 +125,7 @@ namespace Dotnet_Ecom.Areas.Customer.Controllers
 			{
 				// it is a regular customer account and we need to capture payment
 				// stripe logic
-				var domain = "https://localhost:7194/";
+				var domain = Request.Scheme + "://" + Request.Host.Value + "/";
 				var options = new SessionCreateOptions
 				{
 					SuccessUrl = domain + $"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
